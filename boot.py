5f0790a0523ef5243843ccf93d6eaa5ab14dbaf2
7vvXi <tibineko923@gmail.com>
2022-08-19 11:51:21 +0900

    add boot

---
+++ boot.py
@@ -0,0 +1,38 @@
+SSID_NAME = "elecom2g-d44659"
+SSID_PASS = "6a5jxurvjxha" 
+
+import utime
+import network
+import webrepl
+import machine
+
+def connect_wifi(ssid, passkey, timeout=10):
+    wifi= network.WLAN(network.STA_IF)
+    if wifi.isconnected():
+        print('already Connected. connect skip')
+        return wifi
+    else :
+        wifi.active(True)
+        count = 0
+        while count < 5:
+            try:
+                wifi.connect(ssid, passkey)
+                break
+            except:
+                utime.sleep(3)
+                count += 1
+        while not wifi.isconnected() and timeout > 0:
+            print('.')
+            utime.sleep(1)
+            timeout -= 1
+    
+    if wifi.isconnected():
+        print('Connected')
+        webrepl.start(password='1234')
+        return wifi
+    else:
+        print('Connection failed!')
+        return ''
+    
+machine.freq(240000000)
+wifi_cdsl = connect_wifi(SSID_NAME, SSID_PASS)
