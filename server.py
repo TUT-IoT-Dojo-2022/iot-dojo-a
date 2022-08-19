316e3d8ad8690edb0a848f6de352138f08c6d2bf
7vvXi <tibineko923@gmail.com>
2022-08-19 15:16:03 +0900

    Add code

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, render_template, jsonify
+import numpy as np
+from scipy.special import ellipe
 import time
 import statistics
 import json
@@ -55,11 +57,52 @@ def inseam_mode():
 
   return jsonify(result_dist)
 
-@app.route("/")
-def clothesDiffSave():#着衣と素肌の差を測ってファイル保存
-  res = 0
-  
-  return jsonify(res)
+@app.route("/waist_circle")
+def waist_circle():
+  #データの処理（円周の代表値を出す）
+  room_yoko = 71 #測定する場所の横幅
+  room_tate = 71 #測定する場所の奥行
+  L1 = 23 #センサーの左側
+  L2 = 26 #センサーの右側
+  L3 = 21 #センサーの背中
+  L4 = 34 #センサーのお腹側
+  daen_yoko = (room_yoko - (L1 + L2)) / 2
+  daen_tate_1 = (room_tate - (L3 + L3)) / 2
+  daen_tate_2 = (room_tate - (L4 + L4)) / 2
+  L = 4 * daen_yoko * ellipe(e)
+  print("f{:.3F}".format(L))
+
+#ウエストの値(左側)を取得し，保存
+@app.route("/waist_left")
+def waist_left():
+  data = request.get_json(force=True)
+  distance = data['distance']
+  dist_db = []
+  for d in distance:
+    dist_cm = int(round(d) / 10.0) 
+    dist_db.append(dist_cm)
+  dist_mode = statistics.mode(dist_db)
+  print("Waist Left: " + str(dist_mode) + "cm")
+  with open("./waist_left.txt", mode="w") as f:
+    f.write(str(dist_mode))
+
+  return jsonify(dist_mode)
+
+#ウエストの値(右側)を取得し，保存
+@app.route("/waist_right")
+def waist_right():
+  data = request.get_json(force=True)
+  distance = data['distance']
+  dist_db = []
+  for d in distance:
+    dist_cm = int(round(d) / 10.0) 
+    dist_db.append(dist_cm)
+  dist_mode = statistics.mode(dist_db)
+  print("Waist Right: " + str(dist_mode) + "cm")
+  with open("./waist_right.txt", mode="w") as f:
+    f.write(str(dist_mode))
+
+  return jsonify(dist_mode)
 
 @app.route("/")
 def shouldwidFront ():#肩幅の計算(正面)
@@ -72,15 +115,9 @@ def shouldwidSide ():#肩幅の計算(側面)
   res = 0
   
   return jsonify(res)
-  
-@app.route("/")
-def westFront ():#ウエストの計算(正面)
-  res = 0
-  
-  return jsonify(res)
 
 @app.route("/")
-def westSide ():#ウエストの計算(側面)
+def clothesDiffSave():#着衣と素肌の差を測ってファイル保存
   res = 0
   
   return jsonify(res)
