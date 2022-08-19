fe8fb8aaadcd436395f07d6485288f9bd0513a61
c0b2108596 <c0b2108596@edu.teu.ac.jp>
2022-08-19 15:26:06 +0900

    新しすぎるウエスト

--- Iot.py
+++ Iot.py
@@ -8,7 +8,10 @@ L2 = 26 #センサーの右側
 L3 = 21 #センサーの背中
 L4 = 34 #センサーのお腹側
 daen_yoko = (room_yoko - (L1 + L2)) / 2
-daen_tate_1 = (room_tate - (L3 + L3)) / 2
-daen_tate_2 = (room_tate - (L4 + L4)) / 2
-L = 4 * daen_yoko * ellipe(e)
-print("f{:.3F}".format(L))
+daen_tate = (room_tate - (L4 + L3)) / 2
+if daen_yoko >= daen_tate:
+    L = 4 * daen_yoko * ellipe(e)
+    print("f{:.3F}".format(L))
+elif daen_yoko < daen_tate:
+    L = 4 * daen_tate * ellipe(e)
+    print("f{:.3F}".format(L))
